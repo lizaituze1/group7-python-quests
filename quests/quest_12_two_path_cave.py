b3ff586b2c2e10202248eba2920fa6921304f58e
shyaka-alex <b.shyaka@alustudent.com>
2026-06-29 00:20:37 +0200

    Add Quest 12 - Two Path Cave

---
+++ quests/quest_12_two_path_cave.py
@@ -0,0 +1,6 @@
+password = "Passwordcracked"
+user_password = input("Please insert your password below: ")
+if user_password == password:
+   print("Access Granted")
+else:
+   print("SIKEEEE Access Denied")
