e1c5c6d68fd736ffa48e8ae0596440dfa1725326
shyaka-alex <b.shyaka@alustudent.com>
2026-06-29 00:45:37 +0200

    Add Quest 14 - Logical Gatekeeper

---
+++ quests/quest_14_logical_gatekeeper.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age"))
+gold_coins=int(input("Enter the amount of your gold coins"))
+if age >=18 and gold_coins >=20:
+     print("You can enter the draw")
+else:
+    print("Sorry you are kiddo, you cant enter the draw")
