323b696776526edc0bb7842e9daa9f49c9da3297
Adit <a.akeen@alustudent.com>
2026-06-28 08:19:08 -0700

    Add Level 4 loop quests

---
+++ quests/quest_16_marching_ants.py
@@ -0,0 +1,4 @@
+# Quest 16: The Marching Ants
+# Use a for loop to print the numbers from 1 to 10.
+for i in range(1, 11):
+    print(i)
---
+++ quests/quest_19_countdown.py
@@ -0,0 +1,5 @@
+# Quest 19: The Countdown
+# Use a for loop with range() to count down from 10 to 1, then print "Blastoff!".
+for i in range(10, 0, -1):
+    print(i)
+print("Blastoff!")
